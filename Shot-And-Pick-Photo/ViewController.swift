994c542d73c6c7817243bdd528e2c073748a57ea
Smith0314 <loyeu314@gmail.com>
2015-04-05 16:18:03 +0800

    Initial Commit

---
+++ Shot-And-Pick-Photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shot-And-Pick-Photo
+//
+//  Created by Smith on 2015/4/5.
+//  Copyright (c) 2015年 Smith-Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
